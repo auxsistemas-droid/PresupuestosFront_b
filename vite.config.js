fea52dc5c574113c8563f4e2282157280d376293
auxsistemas-droid <aux.sistemas@ulsalaguna.edu.mx>
2026-09-14 14:00:20 -0600

    cambios en vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path' // Viene integrado en Node.js
 
 export default defineConfig({
   plugins: [react()],
+  base: '/PresupuestosFront_b/', 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // Le dice a Vite que @ es igual a la carpeta src
